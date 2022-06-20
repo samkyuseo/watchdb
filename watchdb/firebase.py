15d55ab9dfb3365e5ba135a8aeace80c68b74d44
Samuel Lee <samuelkyuseo@gmail.com>
2022-06-19 22:39:45 -0700

    scrapes crown and caliber

---
+++ watchdb/firebase.py
@@ -0,0 +1,137 @@
+import os
+import json
+from typing import Dict, List
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from pathlib import Path
+from datetime import datetime
+
+# Firestore samples: https://cloud.google.com/firestore/docs/samples/
+# Firestore how to guides: https://cloud.google.com/firestore/docs/how-to
+
+
+class JSONOutputParser:
+    cwd = Path(os.getcwd())
+    service_account = cwd / "service_account.json"
+    data_dir = cwd / "data"
+    raw_data_file = data_dir / f"raw-{datetime.today().strftime('%Y-%m-%d')}.json"
+    parsed_data_file = data_dir / f"parsed-{datetime.today().strftime('%Y-%m-%d')}.json"
+
+    raw_data: List[Dict]
+    parsed_data: Dict = {}
+
+    db: firestore._FirestoreClient
+
+    def __init__(self) -> None:
+        """Initialize a JSON Parser object
+
+        1. Initialize connection to firestore
+        2. Create raw_data dictionary from raw data json file
+        """
+        try:
+            # Use a service account
+            cred = credentials.Certificate(self.service_account)
+            firebase_admin.initialize_app(cred)
+            self.db = firestore.client()
+
+            with open(self.raw_data_file, "r") as f:
+                if f != None:
+                    self.raw_data = json.load(f)
+        except Exception as e:
+            print(f"Parser Init Error: {e}")
+
+    def parse(self) -> None:
+        """Parse raw data and save it as a python dict member variable."""
+        for watch in self.raw_data:
+            if watch.get("brand") is None or watch.get("reference") is None:
+                continue
+            # format: brand.reference
+            key = (
+                watch["brand"].lower().replace(" ", "")
+                + "."
+                + watch["reference"].lower()
+            )
+            # Values tied to reference number
+            if self.parsed_data.get(key) == None:
+                self.parsed_data[key] = {
+                    "brand": watch["brand"],
+                    "reference": watch.get("reference"),
+                    "model": watch.get("model"),
+                    "nickname": watch.get("nickname"),
+                    "case_size": watch.get("case_size"),
+                    "movement": watch.get("movement"),
+                    "caliber": watch.get("caliber"),
+                    "power_reserve": watch.get("power_reserve"),
+                    "gender": watch.get("gender"),
+                    "lug_width": watch.get("lug_width"),
+                    "max._wrist_size": watch.get("max._wrist_size"),
+                    "case_thickness": watch.get("case_thickness"),
+                    "price_data": [],
+                }
+            self.parsed_data[key]["price_data"].append(
+                {
+                    "price": watch.get("price"),
+                    "date": watch.get("date"),
+                    "condition": watch.get("condition"),
+                    "url": watch.get("url"),
+                    "box": watch.get("box"),
+                    "paper": watch.get("paper"),
+                    "manual": watch.get("manual"),
+                    "paper_date": watch.get("paper_date"),
+                    "approximate_age": watch.get("approximate_age"),
+                    "dial_color": watch.get("dial_color"),
+                    "year": watch.get("year"),
+                    "case_material": watch.get("case_material"),
+                    "bracelet": watch.get("bracelet"),
+                    "case_back": watch.get("case_back"),
+                }
+            )
+
+    def export_to_file(self) -> None:
+        """Export parsed output to a file."""
+        with open(self.parsed_data_file, "w") as f:
+            if f != None:
+                json.dump(self.parsed_data, f)
+
+    def upload_to_firebase(self) -> None:
+        """Upload parsed watch data to firebase.
+
+        1. If it a new watch, then upload everything (specs + price data)
+        2. If we've already seen it
+            - Just update the price data array with unique price data points
+            - Check for any null specs and try to hydrate
+        """
+        watches_ref = self.db.collection("watches")
+        for id, watch in self.parsed_data.items():
+            doc_ref = watches_ref.document(id)
+            if doc_ref.get().exists:
+                # Only adds unique price_data objects, if not unique, doesn't add them
+                doc_ref.update(
+                    {"price_data": firestore.ArrayUnion(watch["price_data"])}
+                )
+                # Check for any null fields that is not price data and hydrate them
+                watch_specs = doc_ref.get().to_dict()
+                for spec, value in watch_specs.items():
+                    if (
+                        spec != "price_data"
+                        and value is None
+                        and watch[spec] is not None
+                    ):
+                        doc_ref.update({f"{spec}": watch[spec]})
+                        print(f"{id}: {spec} was null. Replaced with {watch[spec]}")
+            else:
+                print(f"Setting new watch: {watch}")
+                doc_ref.set(watch)
+
+
+def main():
+    op = JSONOutputParser()
+    op.parse()
+    op.export_to_file()
+    # op.upload_to_firebase()
+
+
+if __name__ == "__main__":
+    main()
---
+++ watchdb/spiders/crown_and_caliber.py
@@ -0,0 +1,71 @@
+import scrapy
+from datetime import datetime
+import logging
+from scrapy.utils.log import configure_logging
+import os
+from pathlib import Path
+
+
+class CrownAndCaliber(scrapy.Spider):
+    name = "crown_and_caliber"
+    start_urls = [f"https://www.crownandcaliber.com/pages/brands"]
+    user_agent = "Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0"
+    page = 50  # some arbitrary big amount
+
+    configure_logging(install_root_handler=False)
+    logging.basicConfig(
+        filename=Path(os.getcwd())
+        / f"logs/log-{datetime.today().strftime('%Y-%m-%d')}.txt",
+        format="%(levelname)s: %(message)s",
+        level=logging.INFO,
+    )
+
+    def parse(self, response):
+        brands = response.css("a.brand-list--link::attr('href')").getall()
+        for brand in brands:
+            yield response.follow(
+                f"https://www.crownandcaliber.com{brand}", callback=self.parse_brands
+            )
+
+    def parse_brands(self, response):
+        for i in range(1, self.page):
+            yield response.follow(
+                f"{response.request.url}?page={i}",
+                callback=self.parse_page,
+            )
+
+    def parse_page(self, response):
+        links = response.css("a.grid-view-item__link::attr(href)").getall()
+        # Scrape all watch page links and crawl
+        for link in links:
+            yield response.follow(
+                f"https://www.crownandcaliber.com{link}", callback=self.parse_watch
+            )
+
+    def parse_watch(self, response):
+        has_nickname = len(response.css("span.main-product-name::text").getall()) == 2
+        prod_specs = response.css("div.prod-specs > div")
+        specs = {}
+        for ps in prod_specs:
+            key = ps.css("span::text").get()
+            value = ps.css("span.list-value::text").get()
+            if key and value:
+                snake_case_key = (
+                    key.strip().replace("-", "").strip().replace(" ", "_").lower()
+                )
+                value = value.strip().replace("\n        ", "")
+                if value == "Yes" or value == "No":
+                    value = True if value == "Yes" else False
+                specs[snake_case_key] = value
+        yield {
+            "brand": response.css("span.vendor::text").get().strip(),
+            "price": response.css("span.current-price::text").get().strip(),
+            "model": response.css("span.main-product-name::text").get().strip(),
+            "nickname": None
+            if not has_nickname
+            else response.css("span.main-product-name::text").getall()[1].strip(),
+            "reference": response.css("span.model-number::text").get().strip(),
+            "url": response.request.url,
+            "date": datetime.today().strftime("%Y-%m-%d"),
+            **specs,
+        }
